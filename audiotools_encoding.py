ab13c94f67a6e3e54df7dd28f31b341c5fbf5748
Luis Alberto Santana <jackboot7@gmail.com>
2012-02-13 18:48:55 -0430

    First attemp at encoding a wav file using the python-audiotools module.

---
+++ audiotools_encoding.py
@@ -0,0 +1,60 @@
+# -*- config:utf-8 -*-
+
+"""
+Dummy project to test Python Audio Tools encoding capabilities.
+
+"""
+
+
+import audiotools
+
+def print_progress (amount_processed, total_amount):
+    print "%d%%" % (amount_processed * 100 / total_amount)
+
+
+def encode(input_file, progress=print_progress, **kwargs):
+
+    input = audiotools.open(input_file)
+    filename = input_file.rsplit('.', 1)[0]
+
+    # Metadata for output files.
+    meta = audiotools.MetaData()
+    meta.track_name = 'Track Name'
+    
+    # Convert input_file to output formats including metadata
+    
+
+    #   Conversion from WAV to MP3
+    #   The COMPRESSION_MODE[10] corresponds to lame's "--preset
+    #   insane" which is the same as lame's "-b 320" producing 320kbps
+    #   mp3s. 
+    #   
+    input.convert(filename+'.mp3', 
+                    audiotools.MP3Audio,
+                    compression=audiotools.MP3Audio.COMPRESSION_MODES[10],
+                    progress=progress).set_metadata(meta)
+
+
+    #   Conversion from WAV to FLAC
+    #   The COMPRESSION_MODE[8] corresponds to the most amount of
+    #   compression but at the slowest compression speed
+    #
+    input.convert(filename+'.flac', 
+                    audiotools.FlacAudio,
+                    compression=audiotools.FlacAudio.COMPRESSION_MODES[8],
+                    progress=progress).set_metadata(meta)
+
+    #   Conversion from WAV to AAC
+    #   Using the faac binary. The COMPRESSION_MODE[3] corresponds to
+    #   the default value of the faac encoder,  averages at approx.
+    #   120 kbps VBR for a normal stereo input file.
+    #
+    input.convert(filename+'.m4a',
+                    audiotools.M4AAudio_faac,
+                    compression=audiotools.M4AAudio_faac.COMPRESSION_MODES[3],
+                    progress=progress).set_metadata(meta)
+
+
+
+encode('swords.wav')
+
